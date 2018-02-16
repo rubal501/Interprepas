13a460cb3b30620336afec842f547a5de9f3159b
Roberto Rubalcava <rubal820@gmail.com>
2018-02-15 19:00:55 -0600

    Modificacion del controlador de examnen

--- proyecto/CreadorDeExamenes/src/Negocio/Modelo/Examen.java
+++ proyecto/CreadorDeExamenes/src/Negocio/Modelo/Examen.java
@@ -23,6 +23,47 @@ public class Examen {
     public Examen() {
         incisos = new ArrayList<Inciso>();
     }
+
+    public String getClaveAsignatura() {
+        return claveAsignatura;
+    }
+
+    public void setClaveAsignatura(String claveAsignatura) {
+        this.claveAsignatura = claveAsignatura;
+    }
+
+    public String getNombreAsignatura() {
+        return nombreAsignatura;
+    }
+
+    public void setNombreAsignatura(String nombreAsignatura) {
+        this.nombreAsignatura = nombreAsignatura;
+    }
+
+    public String getNombreMaestro() {
+        return nombreMaestro;
+    }
+
+    public void setNombreMaestro(String nombreMaestro) {
+        this.nombreMaestro = nombreMaestro;
+    }
+
+    public String getNombreEscuela() {
+        return nombreEscuela;
+    }
+
+    public void setNombreEscuela(String nombreEscuela) {
+        this.nombreEscuela = nombreEscuela;
+    }
+
+    public List<Inciso> getIncisos() {
+        return incisos;
+    }
+
+    public void setIncisos(List<Inciso> incisos) {
+        this.incisos = incisos;
+    }
+    
 }
 
 /**
